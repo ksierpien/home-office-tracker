16de49baf2be1fcaf7bf91936cd3d5149d4bb4f6
Karol Sierpień <karol.sierpien@comarch.pl>
2021-07-18 20:52:26 +0200

    fix(month): add missing stub day

--- src/app/month/month.component.ts
+++ src/app/month/month.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class MonthComponent implements OnInit {
   weeks = [
     [0, 0, 1, 2, 3, 4, 5],
-    [6, 7, 8, 9, 10, 11],
+    [6, 7, 8, 9, 10, 11, 12],
   ];
 
   constructor() {}
