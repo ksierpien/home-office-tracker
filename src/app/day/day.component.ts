eb2b60d71e76e22993ab8cebb48a6a0996aae213
Karol Sierpień <karol.sierpien@comarch.pl>
2021-07-18 20:03:16 +0200

    feat(day): add day component

---
+++ src/app/day/day.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'hot-day',
+  templateUrl: './day.component.html',
+  styleUrls: ['./day.component.css']
+})
+export class DayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
